42ec7affbd454f8a047be77fb0a9a8825ae67a6b
Felipe Andrés Aravena Avilez <felipe.aravena@landscape.cl>
2024-05-01 14:44:42 -0400

    add proyect

---
+++ android/app/src/main/java/lunch/roulette/MainActivity.java
@@ -0,0 +1,5 @@
+package lunch.roulette;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'lunch.roulette',
+  appName: 'lunch-roulette',
+  webDir: 'www'
+};
+
+export default config;
